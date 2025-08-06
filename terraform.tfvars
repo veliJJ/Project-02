60cb7cad9e56dcb356316ed5cb7e85dfa4fe3474
Velislav <vdzhekov@gmail.com>
2025-08-07 00:12:07 +0200

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_access_key = "AKIAZQ3DPYQPE3FUWZEK"
+aws_secret_key = "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"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+  description = "AKIAZQ3DPYQPE3FUWZEK"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "im8yuv1ZqkZRzmTurYRychG+emS8T6+wMG50DwQ8"
+  type        = string
+  sensitive   = true
+}
+
